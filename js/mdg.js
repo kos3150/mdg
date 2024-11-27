7f8623b0c03ad38442bc54d365ec2469e0fcfc00
KOS3150-sketch <kosmock@asu.edu>
2024-11-27 09:22:56 -0800

    Update index.html link CSS and JavaScript

---
+++ js/mdg.js
@@ -0,0 +1,10 @@
+// Kim Smock JavaScript
+// <script src="script.js"></script>
+// Syntax: add "use strict"; at the top of a script or function.
+"use strict";
+console.log("Strict mode is enabled");
+function myFunction() {
+    "use strict";
+    let x = 3.14;
+    console.log(x);
+}
